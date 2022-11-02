735999f6813e3be046b96165d1dd6363380f4dfe
Aman Bharti <amanbharti0ab@gmail.com>
2022-11-02 21:28:29 +0530

    added without using extra space code

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.java
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -1,10 +1,24 @@
 class Solution {
     public int[] buildArray(int[] nums) {
         int n = nums.length;
-        int [] ans = new int[n];
-        for(int i = 0; i < n; i++){
+        //build an array for update new values, it takes o(n) for both space complexity and time complexity
+        int [] ans = new int[n]; 
+        for(int i = 0; i < n; i++){  
             ans[i] = nums[nums[i]];
         }
         return ans;
+    }
+}
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        // without using extra space, it takes o(1) space complexity and o(n) time complexity
+        for(int i = 0; i < n; i++){  
+            nums[i] = n * (nums[nums[i]] % n) + nums[i];
+        }
+        for(int i = 0; i < n; i++){
+            nums[i] = nums[i]/n;
+        }
+        return nums;
     }
 }
