2b51c4ac18861bef6f0bfb1982c03456bc362c9a
Aman Bharti <91002848+aman0ab@users.noreply.github.com>
2023-01-05 22:38:45 +0530

    Time: 3 ms (46.00%), Space: 41.6 MB (54.89%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,14 +1,16 @@
 class Solution {
     public int heightChecker(int[] heights) {
-       
-       int[] copy = heights.clone();
-        Arrays.sort(copy);
-        int count = 0;
-        for(int i = 0; i < heights.length; ++i){
-            if(heights[i] != copy[i]){
-                count++;
+      int[] bucket = new int[101];
+        for(int height : heights){
+            bucket[height]++;
+        }
+        int c=0,index=0;
+        for(int i=1;i<101;i++){ //iterate the bucket
+            while(bucket[i]>0){ // till count is not 0
+                if(i != heights[index++]) c++; //if it does not match expected height increase count
+                bucket[i]--; // keep decreasing element count in bucket
             }
         }
-        return count;
+        return c;
     }
 }
