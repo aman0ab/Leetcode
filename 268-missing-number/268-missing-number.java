13825cb389c0482dcbc2dfdb53581b6de908f898
Aman Bharti <91002848+aman0ab@users.noreply.github.com>
2023-01-11 17:25:11 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (80.09%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,22 +1,10 @@
 class Solution {
-    public int missingNumber(int[] arr) {
-      int i = 0;
-        while (i < arr.length) {
-            int correctIndex = arr[i];
-            if (arr[i] < arr.length && arr[i] != arr[correctIndex]) {
-                int temp = arr[i];
-                arr[i] = arr[correctIndex];
-                arr[correctIndex] = temp;
-            } else {
-                ++i;
-            }
-        }
-        for (int index = 0; index < arr.length; index++) {
-            if (arr[index] != index){
-                return index;
-            }
-        }
-        return arr.length;
-                                                     
+    public int missingNumber(int[] nums) {
+     int res = nums.length;
+    for(int i=0; i<nums.length; i++){
+        res ^= i;
+        res ^= nums[i];
+    }
+    return res;                                              
     }
 }
